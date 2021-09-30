be60ee9faf7d4a3ba629a267ec602ac71488cc73
dh0106210229 <74976434+dh0106210229@users.noreply.github.com>
2021-09-30 19:21:43 +0800

    Add files via upload

---
+++ Sentry-Vision-main/build/CMakeFiles/Sentry-Vision.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tup/Desktop/Sentry-Vision/AngelSolver/AngleSolver.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/AngelSolver/AngleSolver.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/AngelSolver/Kalman.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/AngelSolver/Kalman.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/Armor/ArmorDetector.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/Armor/ArmorDetector.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/Camera/DaHengCamera.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/Camera/DaHengCamera.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/ImageProcess/ImageProcess.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/ImageProcess/ImageProcess.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/Serial/CRC_Check.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/Serial/CRC_Check.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/Serial/serialport.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/Serial/serialport.cpp.o"
+  "/home/tup/Desktop/Sentry-Vision/main.cpp" "/home/tup/Desktop/Sentry-Vision/build/CMakeFiles/Sentry-Vision.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sentry-Vision-main/build/CMakeFiles/Sentry-Vision.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sentry-Vision.dir/main.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/Camera/DaHengCamera.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/ImageProcess/ImageProcess.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/Armor/ArmorDetector.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/AngelSolver/AngleSolver.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/AngelSolver/Kalman.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/Serial/CRC_Check.cpp.o"
+  "CMakeFiles/Sentry-Vision.dir/Serial/serialport.cpp.o"
+  "Sentry-Vision.pdb"
+  "Sentry-Vision"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sentry-Vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
